4b49b3fead979213b09dbfbf2fdd4f4061479d82
tynrare <tynrare@gmail.com>
2022-02-20 17:58:00 +0300

    ver 2

--- src/index.js
+++ src/index.js
@@ -1,40 +1,8 @@
-import WebglWrapper from "./WebglWrapper";
-import frag from './dust-0/shader.frag';
-import vert from './dust-0/shader.vert';
-
-class App {
-  constructor() {
-    active = true;
-  }
-
-  run() {
-    App.instance = new App();
-    this.loop(0);
-  }
-
-  init() {
-    this.webgl = new WebglWrapper();
-    this.webgl.init(frag, vert);
-
-    window.addEventListener("resize", () => {
-      this.webgl.resize();
-    });
-  }
-
-  loop(timestamp) {
-    this.webgl.update(timestamp);
-
-    if (this.active) {
-      requestAnimationFrame(() => this.loop());
-    }
-  }
-}
-
 /**
  * Entry point
  */
 function main() {
-  App.run();
+  //... todo
 }
 
-main();
+document.addEventListener('DOMContentLoaded', main);
